81c30d6344d25a3ea88372b586f3bd26e08ab324
stompdoctor <jonjpeterkin@gmail.com>
2016-09-22 00:12:38 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_hash = {}
+  pigeon_data.each do |attribute, data|
+  	data.each do |value, names|
+  		names.each do |name|
+  			if pigeon_hash[name] == nil
+  				pigeon_hash[name] = {} 
+  				pigeon_hash[name][attribute] = []
+  			elsif pigeon_hash[name][attribute] == nil
+  				pigeon_hash[name][attribute] = []
+  			end
+  			pigeon_hash[name][attribute] << value.to_s
+  		end
+  	end
+  end
+  pigeon_hash
 end
